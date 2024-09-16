6e5797411d22aa72cdf3b94fb99cee3fe588f347
si-shreyaskudav <shreyas.kudav@sportzinteractive.net>
2024-09-16 13:28:04 +0530

    first commit

---
+++ EngineTestAPI/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EngineTestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Ping")]
+        public string Ping()
+        {
+            return "Pong!";
+        }
+    }
+}
